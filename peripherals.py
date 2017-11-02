197961cf699d756d41d1fe9c7207c4e052b9df5f
stoplime <slanimero@gmail.com>
2017-11-02 12:47:32 -0400

    init peripheral system

---
+++ peripherals.py
@@ -0,0 +1,9 @@
+# Demo of the concept of using peripheral vision preprocessing
+# before any convolution to identify object is large images
+import sys
+from os.path import expanduser
+sys.path.insert(0, expanduser("~") + '/python/kerasTools')
+
+import customModel
+
+
